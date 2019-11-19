ce8ee2472a764d091ffd46e773da53eedc41a778
oddjob23 <karaklajicmilos94@gmail.com>
2019-11-19 21:14:20 +0100

    fixed urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomePageView, ProductDetailView, CategorieView
+from .views import HomePageView, ProductDetailView
 app_name = "maksim"
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('detail/<int:pk>', ProductDetailView.as_view(), name='detail'),
-    path('category/<category_name>', CategorieView.as_view(), name='category')
+
 ]
