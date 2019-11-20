2f68c4988086cc46264ed2f2fa73abff88e06852
oddjob23 <karaklajicmilos94@gmail.com>
2019-11-20 01:00:27 +0100

    changed urls.py

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,5 @@ app_name = "maksim"
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('detail/<int:pk>', ProductDetailView.as_view(), name='detail'),
-    path('kolekcija/<str:kolekcija>', CollectionView.as_view(), name='kolekcija')
+    path('kolekcija/<str:slug>', CollectionView.as_view(), name='kolekcija')
 ]
